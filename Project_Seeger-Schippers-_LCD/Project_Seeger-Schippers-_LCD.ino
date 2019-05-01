340d45015e6ecb5cc02f4cbd4ff058762e680520
Kaden Seeger <02kaden.seeger@gulllakecs.org>
2019-05-01 12:22:50 -0400

    Initial commit

---
+++ Project_Seeger-Schippers-_LCD/Project_Seeger-Schippers-_LCD.ino
@@ -0,0 +1,16 @@
+\\*
+5.1.19
+Creators: Kaden Seeger, Riley Schippers
+Lcd screen that turns on and prints "hello" 
+
+*\\
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
